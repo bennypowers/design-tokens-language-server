e52d4f172ded026935d29b4e70b501b5108259f8
Benny Powers <web@bennypowers.com>
2025-04-02 13:50:49 +0300

    feat(zed): bundle linux bin

--- extensions/zed/src/lib.rs
+++ extensions/zed/src/lib.rs
@@ -1,4 +1,5 @@
 extern crate zed_extension_api;
+use std::path::Path;
 use zed_extension_api::{self as zed, LanguageServerId};
 
 struct DesignTokensLanguageserverExtension {
@@ -16,7 +17,12 @@ impl zed::Extension for DesignTokensLanguageserverExtension {
         worktree: &zed::Worktree,
     ) -> Result<zed::Command, std::string::String> {
         Ok(zed::Command {
-            command: "/var/home/bennyp/.local/bin/design-tokens-language-server".to_owned(),
+            // command: "/var/home/bennyp/.local/bin/design-tokens-language-server".to_owned(),
+            command: Path::new(worktree.root_path().as_str())
+                .join("bin/design-tokens-language-server")
+                .into_os_string()
+                .into_string()
+                .unwrap(),
             args: [].to_vec(),
             env: Default::default(),
         })
