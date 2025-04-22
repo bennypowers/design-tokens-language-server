ea2deb86d523c60788c9c608cc8f95e00f182467
Benny Powers <web@bennypowers.com>
2025-04-22 11:39:34 +0300

    chore: refactor build steps

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,42 @@
 import { Server } from "#server";
 
-Server.serve({ io: 'stdio' });
+import { parseArgs } from "@std/cli/parse-args";
+
+const flags = parseArgs(Deno.args, {
+  boolean: ["stdio", "socket"],
+  string: ["version", "port"],
+  default: { stdio: true },
+  negatable: ["color"],
+});
+
+if (flags.version) {
+  console.log("Design tokens language server");
+  console.log("version: 0.0.4");
+  Deno.exit(0);
+}
+
+if (flags.port && !flags.socket) {
+  console.log("Design tokens language server");
+  console.log("usage: --port=<port> --socket");
+  console.log("usage: --stdio");
+  Deno.exit(1);
+}
+
+if (flags.stdio) {
+  Server.serve({ io: "stdio" });
+} else if (flags.socket && flags.port) {
+  const port = parseInt(flags.port);
+
+  if (Number.isNaN(port)) {
+    console.error("Invalid port number");
+    Deno.exit(1);
+  }
+
+  // Server.serve({ io: 'socket', port });
+
+  console.log(`Socket transport not implemented yet. Please use --stdio`);
+  Deno.exit(1);
+} else {
+  console.log("Invalid arguments");
+  Deno.exit(1);
+}
